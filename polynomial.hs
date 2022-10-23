58e36d3f77d5c66e63703398e38d43779a093a6c
Daniel Rodrigues <up202006562@up.pt>
2022-10-23 22:31:47 +0100

    .

--- polynomial.hs
+++ polynomial.hs
@@ -26,10 +26,10 @@ type Polynomial = [Monomial]
 
 ------------------------------- Utils ------------------------------
 
-degree :: Monomial -> Int
+degree :: Monomial -> Int -- Calcula o grau de um monómio
 degree m = foldl (\acc x -> acc + snd x) 0 (snd m)
 
-compareMonomial :: Monomial -> Monomial -> Ordering
+compareMonomial :: Monomial -> Monomial -> Ordering -- Compara dois monómios
 compareMonomial m1 m2
   | d1 > d2 = LT
   | d1 < d2 = GT
@@ -42,60 +42,54 @@ compareMonomial m1 m2
     c1 = fst m1
     c2 = fst m2
 
-sortPolynomial :: Polynomial -> Polynomial
+sortPolynomial :: Polynomial -> Polynomial -- Ordena um polinómio
 sortPolynomial = sortBy compareMonomial
 
-equalLiteral :: Monomial -> Monomial -> Bool
+equalLiteral :: Monomial -> Monomial -> Bool -- Verifica se a parte literal de dois monómios são iguais
 equalLiteral m1 m2 = fromList (snd m1) == fromList (snd m2)
 
-equalMonomial :: Monomial -> Monomial -> Bool
+equalMonomial :: Monomial -> Monomial -> Bool -- Verficia se dois monómios são iguais 
 equalMonomial m1 m2 = fst m1 == fst m2 && equalLiteral m1 m2
 
-equal2Polynomial :: Polynomial -> Polynomial -> Bool
+equal2Polynomial :: Polynomial -> Polynomial -> Bool -- Verifica se dois polinómios são iguais
 equal2Polynomial p1 p2 = length p1 == length p2 && foldl (&&) True (zipWith equalMonomial (sortPolynomial p1) (sortPolynomial p2))
-
---removeZeroCoefficient :: Polynomial -> Polynomial
---removeZeroCoefficient = filter (\x -> fst x /= 0)
-
---removeZeroLiterals :: Polynomial -> Polynomial
---removeZeroLiterals p1 = [(fst m1, filter (\x -> snd x /= 0) (snd m1)) | m1 <- p1]
 
 ------------------------------- normalize -----------------------------------
 
-reduceLiterals :: [Literal] -> [Literal]
+reduceLiterals :: [Literal] -> [Literal] -- Simplifica a parte literal de dois monómios, removendo as letras cujo o expoente é zero e adicionando os expoentes de letras iguais
 reduceLiterals [] = []
 reduceLiterals (x : xs) = if snd a /= 0 then a : b else b
   where
     a = (fst x, snd x + sum [snd y | y <- xs, fst y == fst x])
     b = reduceLiterals ([y | y <- xs, fst y /= fst x])
 
-reduceLiteralsPolynomial :: Polynomial -> Polynomial
+reduceLiteralsPolynomial :: Polynomial -> Polynomial -- Aplica função "reduceLiterals" num polinómio
 reduceLiteralsPolynomial p1 = [(fst y, reduceLiterals (snd y)) | y <- p1, fst y /= 0]
 
-reducePolynomial :: Polynomial -> Polynomial
+reducePolynomial :: Polynomial -> Polynomial -- Simplifica um monómio adicionando monómios cuja a parte é igual e removendo aqueles que têm coefeciente nulo
 reducePolynomial [] = []
 reducePolynomial (x : xs) = if fst a /= 0 then a : b else b
   where
     a = sumListMonomials (x : [y | y <- xs, equalLiteral x y])
     b = reducePolynomial [y | y <- xs, not (equalLiteral x y)]
 
-normalizePolynomial :: Polynomial -> Polynomial
+normalizePolynomial :: Polynomial -> Polynomial -- Normaliza um polinómio
 normalizePolynomial = sortPolynomial . reducePolynomial . reduceLiteralsPolynomial
 
 -------------------------------- sum ----------------------------------------
 
-sumListMonomials :: [Monomial] -> Monomial
+sumListMonomials :: [Monomial] -> Monomial -- Soma uma lista de monómios
 sumListMonomials = foldl1 (\acc x -> if equalLiteral acc x then (fst acc + fst x, snd acc) else error "The literal part of the monomials are different!")
 
-sum2Polynomials :: Polynomial -> Polynomial -> Polynomial
+sum2Polynomials :: Polynomial -> Polynomial -> Polynomial -- Soma dois polinómios
 sum2Polynomials p1 p2 = normalizePolynomial (p1 ++ p2)
 
-sumPolynomials :: [Polynomial] -> Polynomial
+sumPolynomials :: [Polynomial] -> Polynomial -- Soma uma lista de polinómios
 sumPolynomials p1 = normalizePolynomial (foldl1 (++) p1)
 
 -------------------------------- prod ---------------------------------------
 
-reduceProdLiterals :: [Literal] -> [Literal]
+reduceProdLiterals :: [Literal] -> [Literal] --Multiplica a parte literal de dois monómios e simplifica-a
 reduceProdLiterals [] = []
 reduceProdLiterals (x : xs)
   | snd a == 0 = reduceProdLiterals ([y | y <- xs, fst y /= fst x])
@@ -103,37 +97,37 @@ reduceProdLiterals (x : xs)
   where
     a = (fst x, snd x + sum [snd y | y <- xs, fst y == fst x])
 
-prod2Monomials :: Monomial -> Monomial -> Monomial
+prod2Monomials :: Monomial -> Monomial -> Monomial -- Multiplica dois monómios
 prod2Monomials m1 m2 = (fst m1 * fst m2, reduceProdLiterals (snd m1 ++ snd m2))
 
-prod2Polynomials :: Polynomial -> Polynomial -> Polynomial
+prod2Polynomials :: Polynomial -> Polynomial -> Polynomial -- Multiplica dois polinómios
 prod2Polynomials p1 p2 = normalizePolynomial [prod2Monomials x y | x <- p1, y <- p2]
 
-prodPolynomials :: [Polynomial] -> Polynomial
+prodPolynomials :: [Polynomial] -> Polynomial -- Multiplica uma lista de polinómios
 prodPolynomials p1
   | null p1 = []
   | otherwise = foldl1 prod2Polynomials p1
 
 -------------------------------- deriv --------------------------------------
 
-reduceDerivLiterals :: Char -> [Literal] -> [Literal]
+reduceDerivLiterals :: Char -> [Literal] -> [Literal] -- 
 reduceDerivLiterals l l1 = [(i, j -1) | (i, j) <- l1, i == l] ++ [(i, j) | (i, j) <- l1, i /= l]
 
-derivMonomial :: Char -> Monomial -> Monomial
+derivMonomial :: Char -> Monomial -> Monomial -- 
 derivMonomial l m1 = if any (== l) [i | (i, j) <- snd m1] then (fst m1 * head [j | (i, j) <- snd m1, i == l], reduceDerivLiterals l (snd m1)) else (0, [])
 
-derivPolynomial :: Char -> Polynomial -> Polynomial
+derivPolynomial :: Char -> Polynomial -> Polynomial -- 
 derivPolynomial l p1 = normalizePolynomial [derivMonomial l x | x <- normalizePolynomial p1]
 
 -------------------------------- Parse --------------------------------------
 
-outMonomial :: Monomial -> String
+outMonomial :: Monomial -> String -- Transforma um monómio na representação interna numa string
 outMonomial m = (if fst m == 1 then "" else if fst m == -1 then "-" else show (fst m)) ++ concat [if snd x > 1 then fst x : "^" ++ show (snd x) else fst x : "" | x <- snd m]
 
-outPolynomial :: Polynomial -> String
+outPolynomial :: Polynomial -> String -- Transforma um monómio na representação interna numa string
 outPolynomial p1 = foldl (\acc x -> if not (null acc) then (if head x == '-' then acc ++ " - " ++ tail x else acc ++ " + " ++ x) else acc ++ x) "" (map outMonomial p1)
 
-parseLiteral :: String -> [Literal]
+parseLiteral :: String -> [Literal] -- -- Converte uma string que contém uma lista de literais
 parseLiteral "" = []
 parseLiteral s
   | length s == 1 = [(head s, 1)]
@@ -141,12 +135,12 @@ parseLiteral s
   where
     a = if s !! 1 /= '^' then (head s, 1) else (head s, read (takeWhile isDigit (drop 2 s)))
 
-parseMonomial :: String -> Monomial
+parseMonomial :: String -> Monomial -- Converte uma string que contém um monómio
 parseMonomial s = (if a == "" then 1 else read a, parseLiteral (drop (length a) s))
   where
     a = takeWhile (not . isAlpha) s
 
-parsePolynomial :: String -> Polynomial
+parsePolynomial :: String -> Polynomial -- -- Converte uma string que contém um polinómio
 parsePolynomial p1 = parsePolynomialAux (filter (/= ' ') p1)
 
 parsePolynomialAux :: String -> Polynomial
